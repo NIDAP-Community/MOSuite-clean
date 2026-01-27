93c193c0cd79094e408c4c070b1e01b43c4e2c56
kelly-sovacool <kelly.sovacool@nih.gov>
2026-01-27 22:50:14 +0000

    feat: add a few basic params to app panel

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-# Code Ocean - MOSuite - create
+# CodeOcean-MOSuite-clean
 
-create multiOmicDataset from csv/tsv files
+clean raw counts
 
-[Code Ocean Capsule](https://poc-nci.codeocean.io/capsule/2543295/tree)
+- [Code Ocean Capsule](https://poc-nci.codeocean.io/capsule/9989765/tree)
+- [MOSuite R package docs](https://ccbr.github.io/MOSuite/)
--- code/main.R
+++ code/main.R
@@ -10,32 +10,35 @@ library(dplyr)
 results_dir <- file.path('..','results')
 plots_dir <- file.path(results_dir, 'figures')
 options(moo_plots_dir = plots_dir, moo_save_plots = TRUE)
-dir.create(plots_dir, recursive=TRUE)
 
 # log installed packages & versions
 pkg_versions <- tibble::as_tibble(installed.packages())
 write_csv(pkg_versions, file.path(results_dir, 'r-packages.csv'))
 
 # parse CLI arguments
-parser <- ArgumentParser(description = "Create multi-omic dataset from files")
+parser <- ArgumentParser()
 
-parser$add_argument("--sample_metadata_filename", type="character", required=TRUE)
-parser$add_argument("--raw_counts_filename", type="character", required=TRUE)
-parser$add_argument("--delim", type="character", required=FALSE, default=",")
+parser$add_argument("--moo", type="character", required=TRUE)
+parser$add_argument("--cleanup_column_names", type="logical", default=TRUE)
+parser$add_argument("--count_type", type="character", default="raw")
 
 args <- parser$parse_args()
 
 # validate inputs
-for (f in c(args$sample_metadata_filename, args$raw_counts_filename)) {
+for (f in c(args$moo)) {
     if (!file.exists(f)) {
         stop(glue("File not found: {f}"))
     }
 }
+moo <- read_rds(args$moo)
+if (!inherits(moo, 'MOSuite::multiOmicDataSet')) {
+    stop(glue('The input is not a multiOmicDataSet. class: {class(moo)}'))
+}
 
 # run MOSuite
-moo <- create_multiOmicDataSet_from_files(
-    sample_meta_filepath = args$sample_metadata_filename,
-    feature_counts_filepath = args$raw_counts_filename,
-    delim = args$delim
-)
-write_rds(moo, file.path(results_dir, 'moo.rds'))
+moo |> 
+    clean_raw_counts(
+        count_type = args$count_type,
+        cleanup_column_names = args$cleanup_column_names
+        ) |> 
+    write_rds(file.path(results_dir, 'moo.rds'))
