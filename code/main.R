fdf2ba9f7a015bdd29d2755994cc47691f38a3ad
Kelly Sovacool <kelly.sovacool@nih.gov>
2026-01-29 09:21:27 -0500

    feat: add clean_raw_counts() args

--- code/main.R
+++ code/main.R
@@ -20,8 +20,14 @@ write_csv(pkg_versions, file.path(results_dir, 'r-packages.csv'))
 parser <- ArgumentParser()
 
 parser$add_argument("--regex_moo", type="character", default=".*\\.rds$")
+parser$add_argument("--sample_id_colname", type="character", default=NULL, help="Column name for sample IDs")
+parser$add_argument("--feature_id_colname", type="character", default=NULL, help="Column name for feature IDs")
+parser$add_argument("--samples_to_rename", type="character", default="", help="Sample names to rename")
 parser$add_argument("--cleanup_column_names", type="logical", default=TRUE)
 parser$add_argument("--count_type", type="character", default="raw")
+parser$add_argument("--split_gene_name", type="logical", default=TRUE, help="Split gene name column by special characters")
+parser$add_argument("--aggregate_rows_with_duplicate_gene_names", type="logical", default=TRUE, help="Aggregate counts for duplicate gene names")
+parser$add_argument("--gene_name_column_to_use_for_collapsing_duplicates", type="character", default="", help="Column to use for collapsing duplicates")
 
 args <- parser$parse_args()
 
@@ -43,6 +49,12 @@ if (!inherits(moo, 'MOSuite::multiOmicDataSet')) {
 moo |> 
     clean_raw_counts(
         count_type = args$count_type,
-        cleanup_column_names = args$cleanup_column_names
+        sample_id_colname = args$sample_id_colname,
+        feature_id_colname = args$feature_id_colname,
+        samples_to_rename = args$samples_to_rename,
+        cleanup_column_names = args$cleanup_column_names,
+        split_gene_name = args$split_gene_name,
+        aggregate_rows_with_duplicate_gene_names = args$aggregate_rows_with_duplicate_gene_names,
+        gene_name_column_to_use_for_collapsing_duplicates = args$gene_name_column_to_use_for_collapsing_duplicates
         ) |> 
     write_rds(file.path(results_dir, 'moo', 'moo.rds'))
